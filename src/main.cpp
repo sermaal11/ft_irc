ad2a1af03b5661cf30b3035bb1f94cd417223900
Jariskan <sergio.marin.alvarez@gmail.com>
2026-02-05 10:07:03 +0100

    feat: add English and Spanish READMEs and enhance port argument validation

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,17 @@
 /*   By: sergio <sergio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 09:13:18 by sergio            #+#    #+#             */
-/*   Updated: 2026/02/05 09:44:55 by sergio           ###   ########.fr       */
+/*   Updated: 2026/02/05 09:54:36 by sergio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <cstdlib>  //std::atoi
-#include <iostream> //std::cerr
-#include <string>   //std::string (kept for std::string password)
+# include <cstdlib>  //std::atoi
+# include <iostream> //std::cerr
+# include <string>   //std::string (kept for std::string password)
+# include <cctype>   //std::isdigit
 
-#define RED "\033[1;31m"
-#define RESET "\033[0m"
+# define RED "\033[1;31m"
+# define RESET "\033[0m"
 
 int main(int argc, char **argv) 
 {
@@ -24,8 +25,17 @@ int main(int argc, char **argv)
     return (1);
   }
 
+  for (int i = 0; argv[1][i]; i++)
+  {
+    if (!isdigit(argv[1][i]))
+    {
+      std::cerr << RED << "<port> must contain only digits" << RESET << "\n";
+      return (1);
+    }
+  }
+  
   int port = std::atoi(argv[1]);
-  if (port <= 0 || port > 65535) 
+  if (port < 0 || port > 65535) 
   {
     std::cerr << RED << "Invalid port number" << RESET << "\n";
     return (1);
