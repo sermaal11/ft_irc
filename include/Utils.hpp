07fc63a378c4989b3083fee0bf4fa4f4d2dd906a
Jariskan <sergio.marin.alvarez@gmail.com>
2026-02-05 14:59:10 +0100

    feat: initialize server socket with SO_REUSEADDR and non-blocking mode, and add necessary socket-related headers.

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -6,17 +6,23 @@
 /*   By: sergio <sergio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 13:00:00 by sergio            #+#    #+#             */
-/*   Updated: 2026/02/05 12:58:38 by sergio           ###   ########.fr       */
+/*   Updated: 2026/02/05 14:57:05 by sergio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
-#include <cctype>
-#include <cstdlib>
-#include <iostream>
-#include <string>
+#include <cctype>			// isdigit
+#include <cstdlib>			// std::atoi
+#include <iostream>			// std::cerr, std::cout, std::endl
+#include <string>			// std::string
+#include <sys/types.h>		// socket, bind, listen, accept, connect, send, recv, close
+#include <sys/socket.h>		// socket, bind, listen, accept, connect, send, recv, close
+#include <unistd.h>     	// close
+#include <cerrno>			// errno
+#include <cstring>      	// strerror
+#include <fcntl.h>      	// fcntl, O_NONBLOCK
 
 #define RED "\033[1;31m"
 #define GREEN "\033[1;32m"
--- src/Server.cpp
+++ src/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: sergio <sergio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 12:48:06 by sergio            #+#    #+#             */
-/*   Updated: 2026/02/05 13:02:36 by sergio           ###   ########.fr       */
+/*   Updated: 2026/02/05 14:58:25 by sergio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,31 @@ Server::Server(int port, std::string &password)
 
 void Server::run() 
 {
-  std::cout << GREEN << "Starting IRC server on port " << _port << RESET
-            << std::endl;
+	int serverFd = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (serverFd < 0) 
+	{
+		std::cerr << RED << "socket() failed: " << std::strerror(errno)
+		<< RESET << "\n";
+		return;
+	}
+	
+	int opt = 1;
+	if (::setsockopt(serverFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) 
+	{
+    	std::cerr << RED << "setsockopt(SO_REUSEADDR) failed: " << std::strerror(errno) << RESET << "\n";
+    	::close(serverFd);
+    	return;
+	}
+
+	if (fcntl(serverFd, F_SETFL, O_NONBLOCK) < 0) 
+	{
+    	std::cerr << RED << "fcntl(O_NONBLOCK) failed: " << std::strerror(errno) << RESET << "\n";
+    	::close(serverFd);
+    	return;
+	}	
+	
+	std::cout << GREEN << "OK: SO_REUSEADDR enabled" << RESET << "\n";
+	std::cout << GREEN << "OK: server socket created for port " << _port << " (fd=" << serverFd << ")" << RESET << "\n";
+
+    ::close(serverFd);	// ! PROVISIONAL: solo valida socket()
 }
