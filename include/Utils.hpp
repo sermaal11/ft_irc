d785965bd93822f8147952756f8c32412fa776c8
Jariskan <sergio.marin.alvarez@gmail.com>
2026-02-05 17:01:58 +0100

    docs: add five standar colors for messages

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -6,7 +6,7 @@
 /*   By: sergio <sergio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 13:00:00 by sergio            #+#    #+#             */
-/*   Updated: 2026/02/05 16:48:24 by sergio           ###   ########.fr       */
+/*   Updated: 2026/02/05 17:01:26 by sergio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,11 @@
 #include <cstring>      	// strerror
 #include <fcntl.h>      	// fcntl, O_NONBLOCK
 
-#define RED "\033[1;31m"
-#define GREEN "\033[1;32m"
-#define RESET "\033[0m"
+#define RED     "\033[1;31m"
+#define GREEN   "\033[1;32m"
+#define YELLOW  "\033[1;33m"
+#define BLUE    "\033[1;34m"
+#define PURPLE  "\033[1;35m"
+#define RESET   "\033[0m"
 
 #endif
